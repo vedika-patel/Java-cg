1db691c87057cedebbb353fe90119745b38f9321
vedika patel <vedika.patel.cg@gmail.com>
2026-09-16 23:32:35 +0530

    add the file

---
+++ js-operators-assignment.js
@@ -0,0 +1,147 @@
+//part A:
+//1.Addication:
+//Q-1:
+let scecondClassCollected=2500;
+let oneClassCollected=12500;
+let total=scecondClassCollected+oneClassCollected;
+console.log("Total school collection:",total,"rs");
+//Q-2
+let pageFirstTime=18;
+let pageSecondTime=25;
+totalPage=pageFirstTime+pageSecondTime;
+console.log("total read page in today:",totalPage);
+//Q-3
+let mondayItems=125;
+let tuesdayItems=178;
+let totalItems=mondayItems+tuesdayItems;
+console.log("total items sold is:",totalItems);
+//2.Subtraction:
+//Q-1
+let busSeats=80;
+let occupiedSeats=53;
+let emptySeats=busSeats-occupiedSeats;
+console.log("total empty seats=",emptySeats);
+//Q-2
+let studentMarks=500;
+let loseMarks=35;
+let finalMarks=studentMarks-loseMarks;
+console.log("finalmarks=",finalMarks);
+//Q-3
+let boxes=2500;
+let sendBoxes=875;
+let remainingBoxes=boxes-sendBoxes;
+console.log("remaining boxes=",remainingBoxes);
+//3.Multiplication
+//Q-1
+let noteBookCost=45;
+let noteBooks=8;
+let totalCost=noteBookCost*noteBooks;
+console.log("total notebook cost=",totalCost);
+//Q-2
+let bottals=120;
+let hours=6;
+let products=bottals*hours;
+console.log("caluculat total producations=",products);
+//Q-3
+let row=7;
+let plants=15;
+let totalPlants=row*plants;
+console.log("total numbers of plants=",totalPlants);
+//4.Division
+//Q-1
+let pencils=144;
+let studeans=12;
+let totalpenciles=pencils/studeans;
+console.log("number of pencils each student= ",totalpenciles);
+//Q-2
+let kilometrs=360;
+let hour=6;
+let distance=kilometrs/hours;
+console.log(" distance travelled per hour=",distance,"km");
+//Q-3
+let among=72000;
+let department=9;
+let eachDepartment=among/department;
+console.log("amount received by each department=",department);
+//5.Modulus
+// //Q-1
+let students=53;
+let groups=5;
+let leafstudent=students%groups;
+console.log("the number of students left over=",leafstudent);
+//Q-2
+let candidates=128;
+let boxs=10;
+let unpacked=candidates%boxes;
+console.log("the number of candies left unpacked=",unpacked);
+//Q-3
+let x=9;;
+y= x % 2;
+console.log(y," is odd number");
+//Q-4
+let toys=237;
+let box=6;
+let packingBoxs=toys%box;
+console.log("left after toys=",packingBoxs,"toys");
+//Q-5
+let passengers=40;
+let watingPeople=135;
+let leftPeople=watingPeople%passengers;
+console.log("e left after filling as many full buses as possible=",leftPeople,"peoples");
+//6.Exponentiation
+//Q-1
+let length=6;
+let cube=3;
+let volume=length**cube;
+console.log("volume of cube=",volume,"cm^3");
+//Q-2
+let xX =1;
+let yY =2;
+let z=x*y**4;
+console.log("the number of bacteria =",z);
+//Q-3
+let sieds=9;
+let toTal =sieds**2;
+console.log("cells on each side using =",toTal);
+//Q-4
+let X=5;
+let y=4;
+console.log(x**y);
+//Q-5
+let pixels=1024;
+let totalnum=pixels**2;
+console.log(totalnum);
+//part:B
+//1. Simple Assignment 
+//Q-1
+let age=17;
+console.log("age=",age);
+//Q-2
+let penprice=15;
+console.log(penprice);
+//Q-3
+let dayInvariabel=7;
+console.log(dayInvariabel);
+//Q-4
+let city = himmatnager;
+console.log(city);
+//Q-5
+let piValue=3.14159;
+console.log(piValue);
+//2. Add and Assign
+//Q-1
+let marks=200;
+marks += 35;
+console.log("update total price=",marks,"rs.");
+//Q-2
+let accountSaving=5000;
+accountSaving += 1200;
+console.log("account saving update=",accountSaving,"rs.");
+//Q-3
+let  phoneBattery=45;
+phoneBattery += 35;
+console.log("update battery=",phoneBattery,"%");
+//Q-4
+let points=1250;
+points += 375;
+console,log("update points=",points);
